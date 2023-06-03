efb05be381be1d98350f1e04cd2963ee50f69619
Arshad3721 <124030789+Arshad3721@users.noreply.github.com>
2023-06-03 11:42:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,6 +64,20 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
+DIRECT_GEN_DB = int(os.environ.get("DIRECT_GEN_DB", "-1001620024364"))
+
+DIRECT_GEN_URL = os.environ.get("DIRECT_GEN_URL", "https://streambot-sz4k.onrender.com/") # https://example.com/
+
+DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
+
+STREAM_URL = is_enabled((environ.get('STREAM_URL', "True")), True)
+
+STREAM_API = environ.get("STREAM_API", "8e0961cf2c4a8f61f4d7f2b8fa806816b1fa8dc6")
+
+STREAM_SITE = environ.get("STREAM_SITE", "omegalinks.in")
+
+STREAM_LONG = environ.get("STREAM_LONG", False)
+
 
 SHORTENER_API = environ.get("SHORTENER_API", "23dba83e36939b0508ec55978597295baf3ce09a")
 LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL", False)
