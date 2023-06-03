75059f4553ec3accbb9eeae34a5a4c4868efc418
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-06-03 10:50:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -467,16 +467,3 @@ async def gen_link(log_msg: Message):
     page_link = await short_link( page_link)
     stream_link = await short_link(stream_link)
     return page_link, stream_link
-
-#stream link shortner
-
-shortz = shortzy.Shortzy(#stream link shortner
-shortz = shortzy.Shortzy(STREAM_API, STREAM_SITE)
-async def short_link(link):
-    if STREAM_URL:
-        if  STREAM_LONG =="True" or STREAM_LONG is True:
-            return await shortz.get_quick_link(link)
-        else:
-            return await shortz.convert(link, silently_fail=False)
-    return link)
-
